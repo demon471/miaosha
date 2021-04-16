d488cc981ab1a9c25987cd18b459cd850f1793ed
Demon471 <1021222969@qq.com>
2021-04-16 11:59:56 +0800

    添加swaggerui 和主启动类

---
+++ src/main/java/com/roomio/miaosha/MiaoshaApplication.java
@@ -0,0 +1,18 @@
+package com.roomio.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author biqiang
+ * @version 2021/4/16
+ * @Description:
+ * @Copyright (C)2021, Pcitech
+ */
+
+@SpringBootApplication
+public class MiaoshaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( MiaoshaApplication.class, args );
+    }
+}
