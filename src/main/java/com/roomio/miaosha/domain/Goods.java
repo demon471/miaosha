66961e21218451e806aba61605d05a9601c7bd8f
Demon471 <1021222969@qq.com>
2021-04-22 17:43:52 +0800

    添加mybatis-generator 生成相关文件

---
+++ src/main/java/com/roomio/miaosha/domain/Goods.java
@@ -0,0 +1,75 @@
+package com.roomio.miaosha.domain;
+
+import java.math.BigDecimal;
+
+public class Goods {
+    private Integer id;
+
+    private String goodsName;
+
+    private String goodsTitle;
+
+    private String goodsImg;
+
+    private BigDecimal goodsPrice;
+
+    private Integer goodStock;
+
+    private String goodsDetail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName == null ? null : goodsName.trim();
+    }
+
+    public String getGoodsTitle() {
+        return goodsTitle;
+    }
+
+    public void setGoodsTitle(String goodsTitle) {
+        this.goodsTitle = goodsTitle == null ? null : goodsTitle.trim();
+    }
+
+    public String getGoodsImg() {
+        return goodsImg;
+    }
+
+    public void setGoodsImg(String goodsImg) {
+        this.goodsImg = goodsImg == null ? null : goodsImg.trim();
+    }
+
+    public BigDecimal getGoodsPrice() {
+        return goodsPrice;
+    }
+
+    public void setGoodsPrice(BigDecimal goodsPrice) {
+        this.goodsPrice = goodsPrice;
+    }
+
+    public Integer getGoodStock() {
+        return goodStock;
+    }
+
+    public void setGoodStock(Integer goodStock) {
+        this.goodStock = goodStock;
+    }
+
+    public String getGoodsDetail() {
+        return goodsDetail;
+    }
+
+    public void setGoodsDetail(String goodsDetail) {
+        this.goodsDetail = goodsDetail == null ? null : goodsDetail.trim();
+    }
+}
