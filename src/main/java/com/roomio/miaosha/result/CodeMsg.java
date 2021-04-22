f1246f82cd384eff83191a0c23375db50bffb423
Demon471 <1021222969@qq.com>
2021-04-22 11:36:02 +0800

    添加自定义校验和全局异常捕获，返回特定的异常内容

--- src/main/java/com/roomio/miaosha/result/CodeMsg.java
+++ src/main/java/com/roomio/miaosha/result/CodeMsg.java
@@ -7,7 +7,7 @@ public class CodeMsg {
     //通用异常
     public static CodeMsg SUCCESS = new CodeMsg(0, "success");
     public static CodeMsg SERVER_ERROR = new CodeMsg(500100, "服务端异常");
-    public static CodeMsg BIND_ERROR = new CodeMsg(500101, "参数校验异常：%s");
+    public static CodeMsg BIND_ERROR = new CodeMsg(500101, "%s");
     //登录模块 5002XX
     public static CodeMsg PASSWORD_EMPTY = new CodeMsg(500201, "密码为空");
     public static CodeMsg MOBILE_EMPTY = new CodeMsg(500202, "手机号为空");
