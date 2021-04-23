48f1ce22a0af62980f708266011783cb78987641
Demon471 <1021222969@qq.com>
2021-04-23 11:02:37 +0800

    修改Mapper 为XML文件

---
+++ src/main/java/com/roomio/miaosha/dao/GoodsMapper.java
@@ -0,0 +1,27 @@
+package com.roomio.miaosha.dao;
+
+import com.roomio.miaosha.domain.Goods;
+import com.roomio.miaosha.vo.GoodsVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface GoodsMapper {
+
+    List<GoodsVo>  getGoodsVoList();
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Goods record);
+
+    int insertSelective(Goods record);
+
+    Goods selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Goods record);
+
+    int updateByPrimaryKeyWithBLOBs(Goods record);
+
+    int updateByPrimaryKey(Goods record);
+}
---
+++ src/main/java/com/roomio/miaosha/dao/MiaoshaGoodsMapper.java
@@ -0,0 +1,17 @@
+package com.roomio.miaosha.dao;
+
+import com.roomio.miaosha.domain.MiaoshaGoods;
+
+public interface MiaoshaGoodsMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(MiaoshaGoods record);
+
+    int insertSelective(MiaoshaGoods record);
+
+    MiaoshaGoods selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(MiaoshaGoods record);
+
+    int updateByPrimaryKey(MiaoshaGoods record);
+}
